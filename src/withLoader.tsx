b9d237b678d116dc9905360fd05c389c0691056b
rizovs <sergey.rizovs@3m5.de>
2018-11-26 21:12:19 +0100

    Only show loader if asynchronous task lasts longer than 400ms

--- src/withLoader.tsx
+++ src/withLoader.tsx
@@ -1,15 +1,34 @@
-import React, { ComponentType } from 'react';
+import React, { ComponentType, useEffect, useState } from 'react';
 
 interface WithLoaderProps {
   isFetching?: boolean;
 }
 
+const SHOW_LOADER_DELAY = 400;
+
 function withLoader<P>(WrappedComponent: ComponentType<P>) {
   return function (props: P & WithLoaderProps) {
     const { isFetching, ...newProps } = props as WithLoaderProps;
 
-    return isFetching
-      ? <span>fetching...</span>
+    const [isLoaderVisible, setLoaderVisible] = useState(false);
+
+    useEffect(() => {
+      let timer: NodeJS.Timer;
+
+      // Only show loader if asynchronous task lasts longer than 400ms
+      if (isFetching) {
+        timer = setTimeout(() => {
+          setLoaderVisible(true);
+        }, SHOW_LOADER_DELAY);
+      }
+
+      return () => {
+        clearTimeout(timer);
+      };
+    }, [isFetching]);
+
+    return isLoaderVisible
+      ? <span>loading...</span>
       : <WrappedComponent {...newProps}/>;
   }
 }
